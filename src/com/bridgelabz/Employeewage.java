29f36f3a3099bde56b0e3543d826772fab10a70f
ajay812998 <ajay812998@gmail.com>
2021-01-10 23:06:34 +0530

    Added : Until Condition is reached

--- src/com/bridgelabz/Employeewage.java
+++ src/com/bridgelabz/Employeewage.java
@@ -5,12 +5,17 @@ public class Employeewage {
 		public static final int IS_PART_TIME = 2;
 		public static final int EMP_RATE_PER_HOUR = 20;
 		public static final int NUM_OF_WORKING_DAYS = 20;
+		public static final int MAX_HRS_IN_MONTH = 100;
 		
 		public static void main(String[] args) {
 		int empHrs = 0;
 		int empwage = 0;
 		int totalEmpWage = 0;
-		for (int i = 0; i < NUM_OF_WORKING_DAYS; i++) {
+		int totalWorkingDays = 0;
+		int totalEmpHrs = 0;
+		
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
 		int attendance = (int) Math.floor(Math.random() * 10) % 3;
 		switch (attendance) {
 		case 0:
@@ -28,9 +33,10 @@ public class Employeewage {
 		         empHrs = 4;
 		         break;
 		}
-		empwage = EMP_RATE_PER_HOUR * empHrs;
-		totalEmpWage = totalEmpWage + empwage;
+		totalEmpHrs = totalEmpHrs + empHrs;
+		System.out.println("Day: " + totalWorkingDays + "Emp Hr: " + empHrs);
 	  }
-	System.out.println("Employee's salary for the day is " + totalEmpWage);
+	totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+	System.out.println("Employee's salary for the Month is " + totalEmpWage);
 	}
 }
